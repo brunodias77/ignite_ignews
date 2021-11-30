ea64ce4b5d7ea3fc57da8b024bafbeb370b74068
Bruno Dias <brunohdias95@gmail.com>
2021-11-30 13:39:16 -0300

    ignite

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,22 @@
+import { FaGithub } from "react-icons/fa";
+import styles from "./styles.module.scss";
+import { FiX } from "react-icons/fi";
+import { signIn, useSession, signOut } from "next-auth/client";
+
+export function SignInButton() {
+  const [session] = useSession();
+  console.log(session);
+
+  return session ? (
+    <button className={styles.signInButton} onClick={() => signOut()}>
+      <FaGithub color="#04de61" />
+      {session.user.name}
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton} onClick={() => signIn("github")}>
+      <FaGithub color="#eba417" />
+      Sign with github
+    </button>
+  );
+}
